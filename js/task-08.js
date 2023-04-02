749d831a8720aed043a57c99ae015fb637ecc979
Olena <kovalchuk.e.u@gmail.com>
2023-04-02 22:02:04 +0300

    начала делать 8 задачу

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
 
+const form = document.querySelector('.login-form')
+form.addEventListener('submit', onFormSubmit);
+function onFormSubmit(event) {
+event.preventDefault();
+const inputs = document.querySelectorAll('input');
+inputs.forEach(input => {
+    if (input.value === '') { 
+    alert("Все поля должны быть заполнены")
+    }
+});
+
+const formElements = event.currentTarget.elements;
+const mail = formElements.email.value;
+const password = formElements.password.value;
+console.log(mail, password);
+const formData = { mail, password};
+document.querySelector('.login-form').reset();
+};

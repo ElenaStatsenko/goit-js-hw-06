d2f2ea16808a4a8a61a833d4b34aa3b69be5e321
ElenaStatsenko <118728440+ElenaStatsenko@users.noreply.github.com>
2023-04-03 16:45:59 +0300

    сделала 8 задачу

--- js/task-08.js
+++ js/task-08.js
@@ -1,19 +1,29 @@
 
 const form = document.querySelector('.login-form')
+function validation (form) {
+    let result = true;
+    const allInputs = form.querySelectorAll('input');
+    for ( const input of allInputs) {
+if (input.value === "") {
+    alert('Предупреждение о том, что все поля должны быть заполнены');
+    result = false;
+}
+}
+return result;
+}
 form.addEventListener('submit', onFormSubmit);
 function onFormSubmit(event) {
 event.preventDefault();
-const inputs = document.querySelectorAll('input');
-inputs.forEach(input => {
-    if (input.value === '') { 
-    alert("Все поля должны быть заполнены")
-    }
-});
-
+if (validation(this) === true) {
 const formElements = event.currentTarget.elements;
 const mail = formElements.email.value;
 const password = formElements.password.value;
 console.log(mail, password);
 const formData = { mail, password};
-document.querySelector('.login-form').reset();
-};
+ document.querySelector('.login-form').reset(); 
+}
+}
+
+
+
+

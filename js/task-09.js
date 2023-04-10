14e758dab88212575aa8b4ba779fe146927cda91
ElenaStatsenko <118728440+ElenaStatsenko@users.noreply.github.com>
2023-04-10 10:44:13 +0300

    не знаю что в 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,14 @@
+const button = document.querySelector('.change-color');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};

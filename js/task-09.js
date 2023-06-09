d59a38f5196139590f932afcbafdbce71ee3fa56
ElenaStatsenko <118728440+ElenaStatsenko@users.noreply.github.com>
2023-06-09 12:14:37 +0300

    f

--- js/task-09.js
+++ js/task-09.js
@@ -1,19 +1,14 @@
-const button = document.querySelector('.change-color');
+
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
-<<<<<<< HEAD
-
-
-
 
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
-=======
 const  button = document.querySelector('.change-color');
 const span = document.querySelector('.color');
 button.addEventListener('click', () => {
@@ -21,4 +16,4 @@ button.addEventListener('click', () => {
   document.body.style.backgroundColor = hexcolor;
   span.textContent = hexcolor ;
 })
->>>>>>> 78ff05b9c7bdea13feb73e7c06a03e777fb2e7a6
+
